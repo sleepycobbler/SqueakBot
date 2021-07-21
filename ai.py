0069be553882b0d1168276064d7b0b7fb48f5b35
Max Schuhmacher <sleepycobbler@gmail.com>
2021-07-21 12:42:44 -0600

    started foundation

--- ai.py
+++ ai.py
@@ -1,23 +1,19 @@
 import tensorflow as tf
-from tensorflow.keras.layers.experimental import keras_preprocessing
-from tensorflow.keras import layers
-from tensorflow.keras.layers.preprocessing import text
-
-keras.layers.experimental.preprocessing.StringLookup(
-    max_tokens=None, num_oov_indices=1, mask_token=None,
-    oov_token='[UNK]', vocabulary=None, encoding=None, invert=False,
-    output_mode=index_lookup.INT, sparse=False, pad_to_max_tokens=False, **kwargs
-)
-
-import nltk
-nltk.download('punkt')
+from tensorflow import keras
+from tensorflow.keras.layers.experimental import preprocessing
 
 import numpy as np
 import os
 import time
+
+import nltk
+
+nltk.download('punkt')
+
 import random
 
 import filer
+
 
 async def ai_gen_nltk(ctx):
     data = filer.read()
@@ -29,7 +25,9 @@ async def ai_gen_nltk(ctx):
                 for word in nltk.word_tokenize(message_alt):
                     words.append(word)
     ben_text = nltk.text.Text(words)
-    await ctx.channel.send(ben_text.generate(length=random.randrange(5, 100), random_seed=random.randint(0, 9999999999999999999999)))
+    await ctx.channel.send(
+        ben_text.generate(length=random.randrange(5, 100), random_seed=random.randint(0, 9999999999999999999999)))
+
 
 async def ai_gen_tf(ctx):
     data = filer.read()
@@ -37,21 +35,12 @@ async def ai_gen_tf(ctx):
     text = "\n".join(data[id])
 
     vocab = sorted(set(text))
-
+    print(vocab)
     example_texts = ['abcdefg', 'xyz']
 
-    chars = tf.strings.unicode_split(example_texts, input_encoding='UTF-8')
-
-    ids_from_chars = tf.keras.preprocessing.text.text_to_word_sequence(vocabulary=list(vocab), mask_token=None)
-
-    ids = ids_from_chars(chars)
-
-    chars_from_ids = tf.keras.layers.experimental.preprocessing.StringLookup(
-        vocabulary=ids_from_chars.get_vocabulary(), invert=True, mask_token=None)
-
-    chars = chars_from_ids(ids)
-
-    tf.strings.reduce_join(chars, axis=-1).numpy()
+    ids_from_chars = preprocessing.StringLookup(vocabulary=list(vocab))
+    chars_from_ids = tf.keras.layers.experimental.preprocessing.StringLookup(vocabulary=ids_from_chars.get_vocabulary(),
+                                                                             invert=True)
 
     def text_from_ids(ids):
         return tf.strings.reduce_join(chars_from_ids(ids), axis=-1)
@@ -61,9 +50,8 @@ async def ai_gen_tf(ctx):
     ids_dataset = tf.data.Dataset.from_tensor_slices(all_ids)
 
     seq_length = 100
-    examples_per_epoch = len(text)//(seq_length+1)
 
-    sequences = ids_dataset.batch(seq_length+1, drop_remainder=True)
+    sequences = ids_dataset.batch(seq_length + 1, drop_remainder=True)
 
     def split_input_target(sequence):
         input_text = sequence[:-1]
@@ -81,22 +69,17 @@ async def ai_gen_tf(ctx):
 
     dataset = (
         dataset
-        .shuffle(BUFFER_SIZE)
-        .batch(BATCH_SIZE, drop_remainder=True)
-        .prefetch(tf.data.experimental.AUTOTUNE))
-
-    # Length of the vocabulary in chars
-    vocab_size = len(vocab)
-    embedding_dim = 256
-    rnn_units = 1024
+            .shuffle(BUFFER_SIZE)
+            .batch(BATCH_SIZE, drop_remainder=True)
+            .prefetch(tf.data.experimental.AUTOTUNE))
 
     class MyModel(tf.keras.Model):
         def __init__(self, vocab_size, embedding_dim, rnn_units):
             super().__init__(self)
             self.embedding = tf.keras.layers.Embedding(vocab_size, embedding_dim)
             self.gru = tf.keras.layers.GRU(rnn_units,
-                                   return_sequences=True,
-                                   return_state=True)
+                                           return_sequences=True,
+                                           return_state=True)
             self.dense = tf.keras.layers.Dense(vocab_size)
 
         def call(self, inputs, states=None, return_state=False, training=False):
@@ -111,7 +94,11 @@ async def ai_gen_tf(ctx):
                 return x, states
             else:
                 return x
-    
+
+    vocab_size = len(ids_from_chars.get_vocabulary())
+    embedding_dim = 256
+    rnn_units = 1024
+
     model = MyModel(
         vocab_size=len(ids_from_chars.get_vocabulary()),
         embedding_dim=embedding_dim,
@@ -121,80 +108,107 @@ async def ai_gen_tf(ctx):
         example_batch_predictions = model(input_example_batch)
         print(example_batch_predictions.shape, "# (batch_size, sequence_length, vocab_size)")
 
+    model.summary()
+
     sampled_indices = tf.random.categorical(example_batch_predictions[0], num_samples=1)
     sampled_indices = tf.squeeze(sampled_indices, axis=-1).numpy()
 
     loss = tf.losses.SparseCategoricalCrossentropy(from_logits=True)
-
-    example_batch_loss = loss(target_example_batch, example_batch_predictions)
-    mean_loss = example_batch_loss.numpy().mean()
-
-    tf.exp(mean_loss).numpy()
-
     model.compile(optimizer='adam', loss=loss)
 
-    # Directory where the checkpoints will be saved
-    checkpoint_dir = './training_checkpoints'
-    # Name of the checkpoint files
-    checkpoint_prefix = os.path.join(checkpoint_dir, "ckpt_{epoch}")
+    if os.path.isfile("my_model\{saved_model.pbtxt|saved_model.pb}"):
+        class OneStep(tf.keras.Model):
+            def __init__(self, model, chars_from_ids, ids_from_chars, temperature=1.0):
+                super().__init__()
+                self.temperature = temperature
+                self.model = model
+                self.chars_from_ids = chars_from_ids
+                self.ids_from_chars = ids_from_chars
 
-    checkpoint_callback = tf.keras.callbacks.ModelCheckpoint(
-        filepath=checkpoint_prefix,
-        save_weights_only=True)
+                # Create a mask to prevent "" or "[UNK]" from being generated.
+                skip_ids = self.ids_from_chars(['', '[UNK]'])[:, None]
+                sparse_mask = tf.SparseTensor(
+                    # Put a -inf at each bad index.
+                    values=[-float('inf')] * len(skip_ids),
+                    indices=skip_ids,
+                    # Match the shape to the vocabulary
+                    dense_shape=[len(ids_from_chars.get_vocabulary())])
+                self.prediction_mask = tf.sparse.to_dense(sparse_mask)
 
-    EPOCHS = 20
+        model = keras.models.load_model("my_model")
 
-    history = model.fit(dataset, epochs=EPOCHS, callbacks=[checkpoint_callback])
+        one_step_model = OneStep(model, chars_from_ids, ids_from_chars)
+
+        # Now, let's generate a 1000 character chapter by giving our model "Chapter 1"
+        # as its starting text
+        states = None
+        next_char = tf.constant(['Dude'])
+        result = [next_char]
+
+        for n in range(1000):
+            next_char, states = one_step_model.generate_one_step(next_char, states=states)
+            result.append(next_char)
+
+        result = tf.strings.join(result)
+
+        # Print the results formatted.
+        print(result[0].numpy().decode('utf-8'))
+        await ctx.channel.send(result[0].numpy().decode('utf-8'))
+        return
+
+    history = model.fit(dataset, epochs=20)
+
+    model.save("model_one")
 
     class OneStep(tf.keras.Model):
         def __init__(self, model, chars_from_ids, ids_from_chars, temperature=1.0):
-          super().__init__()
-          self.temperature = temperature
-          self.model = model
-          self.chars_from_ids = chars_from_ids
-          self.ids_from_chars = ids_from_chars
+            super().__init__()
+            self.temperature = temperature
+            self.model = model
+            self.chars_from_ids = chars_from_ids
+            self.ids_from_chars = ids_from_chars
 
-          # Create a mask to prevent "[UNK]" from being generated.
-          skip_ids = self.ids_from_chars(['[UNK]'])[:, None]
-          sparse_mask = tf.SparseTensor(
-              # Put a -inf at each bad index.
-              values=[-float('inf')]*len(skip_ids),
-              indices=skip_ids,
-              # Match the shape to the vocabulary
-              dense_shape=[len(ids_from_chars.get_vocabulary())])
-          self.prediction_mask = tf.sparse.to_dense(sparse_mask)
+            # Create a mask to prevent "" or "[UNK]" from being generated.
+            skip_ids = self.ids_from_chars(['', '[UNK]'])[:, None]
+            sparse_mask = tf.SparseTensor(
+                # Put a -inf at each bad index.
+                values=[-float('inf')] * len(skip_ids),
+                indices=skip_ids,
+                # Match the shape to the vocabulary
+                dense_shape=[len(ids_from_chars.get_vocabulary())])
+            self.prediction_mask = tf.sparse.to_dense(sparse_mask)
 
         @tf.function
         def generate_one_step(self, inputs, states=None):
-          # Convert strings to token IDs.
-          input_chars = tf.strings.unicode_split(inputs, 'UTF-8')
-          input_ids = self.ids_from_chars(input_chars).to_tensor()
+            # Convert strings to token IDs.
+            input_chars = tf.strings.unicode_split(inputs, 'UTF-8')
+            input_ids = self.ids_from_chars(input_chars).to_tensor()
 
-          # Run the model.
-          # predicted_logits.shape is [batch, char, next_char_logits]
-          predicted_logits, states = self.model(inputs=input_ids, states=states,
-                                                return_state=True)
-          # Only use the last prediction.
-          predicted_logits = predicted_logits[:, -1, :]
-          predicted_logits = predicted_logits/self.temperature
-          # Apply the prediction mask: prevent "[UNK]" from being generated.
-          predicted_logits = predicted_logits + self.prediction_mask
+            # Run the model.
+            # predicted_logits.shape is [batch, char, next_char_logits]
+            predicted_logits, states = self.model(inputs=input_ids, states=states,
+                                                  return_state=True)
+            # Only use the last prediction.
+            predicted_logits = predicted_logits[:, -1, :]
+            predicted_logits = predicted_logits / self.temperature
 
-          # Sample the output logits to generate token IDs.
-          predicted_ids = tf.random.categorical(predicted_logits, num_samples=1)
-          predicted_ids = tf.squeeze(predicted_ids, axis=-1)
+            # Apply the prediction mask: prevent "" or "[UNK]" from being generated.
+            predicted_logits = predicted_logits + self.prediction_mask
 
-          # Convert from token ids to characters
-          predicted_chars = self.chars_from_ids(predicted_ids)
+            # Sample the output logits to generate token IDs.
+            predicted_ids = tf.random.categorical(predicted_logits, num_samples=1)
+            predicted_ids = tf.squeeze(predicted_ids, axis=-1)
 
-          # Return the characters and model state.
-          return predicted_chars, states
+            # Return the characters and model state.
+            return chars_from_ids(predicted_ids), states
 
+    # Create an instance of the character generator
     one_step_model = OneStep(model, chars_from_ids, ids_from_chars)
 
-    start = time.time()
+    # Now, let's generate a 1000 character chapter by giving our model "Chapter 1"
+    # as its starting text
     states = None
-    next_char = tf.constant(['ROMEO:'])
+    next_char = tf.constant(['Chapter 1'])
     result = [next_char]
 
     for n in range(1000):
@@ -202,83 +216,7 @@ async def ai_gen_tf(ctx):
         result.append(next_char)
 
     result = tf.strings.join(result)
-    end = time.time()
-    print(result[0].numpy().decode('utf-8'), '\n\n' + '_'*80)
-    print('\nRun time:', end - start)
 
-    start = time.time()
-    states = None
-    next_char = tf.constant(['ROMEO:', 'ROMEO:', 'ROMEO:', 'ROMEO:', 'ROMEO:'])
-    result = [next_char]
-
-    for n in range(1000):
-        next_char, states = one_step_model.generate_one_step(next_char, states=states)
-        result.append(next_char)
-
-    result = tf.strings.join(result)
-    end = time.time()
-    print(result, '\n\n' + '_'*80)
-    print('\nRun time:', end - start)
-
-    tf.saved_model.save(one_step_model, 'one_step')
-    one_step_reloaded = tf.saved_model.load('one_step')
-
-    states = None
-    next_char = tf.constant(['ROMEO:'])
-    result = [next_char]
-
-    for n in range(100):
-        next_char, states = one_step_reloaded.generate_one_step(next_char, states=states)
-        result.append(next_char)
-
-    print(tf.strings.join(result)[0].numpy().decode("utf-8"))
-
-    class CustomTraining(MyModel):
-        @tf.function
-        def train_step(self, inputs):
-            inputs, labels = inputs
-            with tf.GradientTape() as tape:
-                predictions = self(inputs, training=True)
-                loss = self.loss(labels, predictions)
-            grads = tape.gradient(loss, model.trainable_variables)
-            self.optimizer.apply_gradients(zip(grads, model.trainable_variables))
-            return {'loss': loss}
-
-    model = CustomTraining(
-    vocab_size=len(ids_from_chars.get_vocabulary()),
-    embedding_dim=embedding_dim,
-    rnn_units=rnn_units)
-
-    model.compile(optimizer = tf.keras.optimizers.Adam(),
-              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True))
-
-    model.fit(dataset, epochs=1)
-
-    EPOCHS = 10
-
-    mean = tf.metrics.Mean()
-
-    for epoch in range(EPOCHS):
-        start = time.time()
-
-        mean.reset_states()
-        for (batch_n, (inp, target)) in enumerate(dataset):
-            logs = model.train_step([inp, target])
-            mean.update_state(logs['loss'])
-
-            if batch_n % 50 == 0:
-                template = f"Epoch {epoch+1} Batch {batch_n} Loss {logs['loss']:.4f}"
-                print(template)
-
-        # saving (checkpoint) the model every 5 epochs
-        if (epoch + 1) % 5 == 0:
-            model.save_weights(checkpoint_prefix.format(epoch=epoch))
-
-        print()
-        print(f'Epoch {epoch+1} Loss: {mean.result().numpy():.4f}')
-        print(f'Time taken for 1 epoch {time.time() - start:.2f} sec')
-        print("_"*80)
-
-    model.save_weights(checkpoint_prefix.format(epoch=epoch))
-
-    pass
+    # Print the results formatted.
+    print(result[0].numpy().decode('utf-8'))
+    await ctx.channel.send(result[0].numpy().decode('utf-8'))
--- grabber.py
+++ grabber.py
@@ -1,6 +1,9 @@
 import random
 import starter
 import filer
+import csv
+import discord
+import profanity
 
 def message_check(message):
     pass
@@ -9,23 +12,41 @@ async def update(ctx):
     id = str(ctx.guild.id)
     channels = ctx.guild.text_channels
     data = filer.read()
+    all_message_data = []
     if id not in data.keys():
         await starter.add_guild(id, data)
     for channel in channels:
         messages = await channel.history(limit=None).flatten() # this single line of code takes 99% longer than any other line here
         for message in messages:
             if len(message.content) > 0:
-                if message.content[0] not in ['-', '!', '>'] and not message.content.startswith("toddbot"):
-                    data[id].append(message.content + '\n')
+                if message.content[0] not in ['-', '!', '>'] and not message.content.startswith("toddbot") and not message.author.bot and "http" not in message.content:
+                    all_message_data.append({"message_id": message.id,
+                                             "author": message.author.name,
+                                             "created_on": message.created_at,
+                                             "edited_on": message.edited_at,
+                                             "num_reactions": len(message.reactions),
+                                             "is_pinned": message.pinned,
+                                             "content": discord.utils.remove_markdown(message.clean_content).encode("ascii", "ignore")})
+                    data[id].append(message.clean_content)
     filer.write(data, id)
+    print('json done')
+    try:
+        with open('data.csv', 'w', newline='') as csvfile:
+            writer = csv.DictWriter(csvfile, fieldnames=['message_id', 'author', 'created_on', 'edited_on', 'num_reactions', 'is_pinned', 'content'])
+            writer.writeheader()
+            for my_data in all_message_data:
+                writer.writerow(my_data)
+            print('csv done')
+    except IOError:
+        print("I/O error")
 
 async def grabber(ctx, links=None):
     id = str(ctx.guild.id)
     data = filer.read()
 
     if id not in data.keys(): # if the guild hasn't used the bot before, we gotta add it to the list
-        starter.add_guild(ctx.guild.id, data)
-        update(ctx)
+        await starter.add_guild(ctx.guild.id, data)
+        await update(ctx)
 
     message = random.choice(data[id])
 
